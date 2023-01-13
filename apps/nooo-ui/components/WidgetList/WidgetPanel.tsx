4af38a40f987dc5e8ebe4f1c97f7808f2ae80983
tolerance-go <892728595@qq.com>
2023-01-14 00:04:41 +0800

    chore

--- apps/nooo-ui/components/WidgetList/WidgetPanel.tsx
+++ apps/nooo-ui/components/WidgetList/WidgetPanel.tsx
@@ -69,7 +69,36 @@ export const WidgetPanel = ({ data }: { data: WidgetData }) => {
                      ) : (
                         <TabletResponsiveSwitcher />
                      )}
+                     <div className='w-9 h-9 flex cursor-pointer transition hover:bg-gray-50 hover:text-sky-600 justify-center items-center border rounded-md'>
+                        <div>
+                           <svg
+                              aria-hidden='true'
+                              data-toggle-icon='moon'
+                              className='w-4 h-4'
+                              fill='currentColor'
+                              viewBox='0 0 20 20'
+                              xmlns='http://www.w3.org/2000/svg'
+                           >
+                              <path d='M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z' />
+                           </svg>
+                           <svg
+                              aria-hidden='true'
+                              data-toggle-icon='sun'
+                              className='w-4 h-4 hidden'
+                              fill='currentColor'
+                              viewBox='0 0 20 20'
+                              xmlns='http://www.w3.org/2000/svg'
+                           >
+                              <path
+                                 d='M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z'
+                                 fillRule='evenodd'
+                                 clipRule='evenodd'
+                              />
+                           </svg>
+                        </div>
+                     </div>
                      <div className='text-gray-200'>|</div>
+
                      <div className='w-9 h-9 flex hover:bg-gray-50 hover:text-amber-500 transition cursor-pointer justify-center items-center border rounded-md'>
                         <svg
                            fill='none'
@@ -174,34 +203,7 @@ export const WidgetPanel = ({ data }: { data: WidgetData }) => {
                      {data.meta.frameworks && (
                         <div className='text-gray-200'>|</div>
                      )}
-                     <div className='w-9 h-9 flex cursor-pointer transition hover:bg-gray-50 hover:text-sky-600 justify-center items-center border rounded-md'>
-                        <div>
-                           <svg
-                              aria-hidden='true'
-                              data-toggle-icon='moon'
-                              className='w-4 h-4'
-                              fill='currentColor'
-                              viewBox='0 0 20 20'
-                              xmlns='http://www.w3.org/2000/svg'
-                           >
-                              <path d='M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z' />
-                           </svg>
-                           <svg
-                              aria-hidden='true'
-                              data-toggle-icon='sun'
-                              className='w-4 h-4 hidden'
-                              fill='currentColor'
-                              viewBox='0 0 20 20'
-                              xmlns='http://www.w3.org/2000/svg'
-                           >
-                              <path
-                                 d='M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z'
-                                 fillRule='evenodd'
-                                 clipRule='evenodd'
-                              />
-                           </svg>
-                        </div>
-                     </div>
+
                      <Link
                         href={`/preview/${data.key}`}
                         target='_blank'
