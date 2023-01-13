c0fa5f5526ecb775ccf2edf6cc83f2c4c1ba62e6
tolerance-go <892728595@qq.com>
2023-01-14 00:01:19 +0800

    chore

--- apps/nooo-ui/components/WidgetList/WidgetPanel.tsx
+++ apps/nooo-ui/components/WidgetList/WidgetPanel.tsx
@@ -208,18 +208,25 @@ export const WidgetPanel = ({ data }: { data: WidgetData }) => {
                         className='w-9 h-9 flex justify-center transition hover:bg-gray-50 hover:text-sky-600 items-center border rounded-md'
                      >
                         <svg
-                           fill='none'
-                           className='w-4 h-4'
-                           stroke='currentColor'
-                           strokeWidth={2}
+                           width='24'
+                           height='24'
                            viewBox='0 0 24 24'
+                           fill='none'
+                           strokeWidth={2}
+                           className='w-4 h-4'
                            xmlns='http://www.w3.org/2000/svg'
-                           aria-hidden='true'
                         >
                            <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25'
+                              d='M20.6599 4L12.1216 12.5382'
+                              stroke='currentColor'
+                           />
+                           <path
+                              d='M20.5085 10.5126V4.15149H14.1475'
+                              stroke='currentColor'
+                           />
+                           <path
+                              d='M11.4969 4H5.666C4.74589 4 4 4.74589 4 5.666V18.9939C4 19.9141 4.74589 20.6599 5.666 20.6599H18.9939C19.9141 20.6599 20.66 19.9141 20.66 18.9939V12.9547'
+                              stroke='currentColor'
                            />
                         </svg>
                      </Link>
