d2a43ee26c7bfa47450d7f2638d8539d95a4d507
tolerance-go <892728595@qq.com>
2023-01-14 17:01:13 +0800

    chore

--- apps/nooo-ui/app/preview/[key]/head.tsx
+++ apps/nooo-ui/app/preview/[key]/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
    return (
       <>
-         <title>NoooUI - 线上 tailwind 设计市场</title>
+         <title>NoooUI - 线上 tailwindcss 设计集散地</title>
          <meta content='width=device-width, initial-scale=1' name='viewport' />
          <link rel='icon' href='/favicon.ico' />
       </>
