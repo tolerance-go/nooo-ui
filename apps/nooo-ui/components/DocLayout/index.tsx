80a97f4dd89cb24dfc936e3569c05037d954cfc5
tolerance-go <892728595@qq.com>
2023-01-14 19:15:48 +0800

    chore

--- apps/nooo-ui/components/DocLayout/index.tsx
+++ apps/nooo-ui/components/DocLayout/index.tsx
@@ -1,4 +1,6 @@
+import clsx from 'clsx'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { PropsWithChildren } from 'react'
 import './prose.css'
 
@@ -9,6 +11,20 @@ export const DocLayout = (
       }
    }>,
 ) => {
+   const pathname = usePathname()
+
+   const routes = [
+      {
+         title: '介绍',
+         children: [
+            {
+               title: '什么是 Nooo-UI？',
+               path: '/docs/introduction/what-is-nooo-ui',
+            },
+         ],
+      },
+   ]
+
    return (
       <div>
          <div className='px-10 py-4 border-b fixed top-0 w-full doc-header flex items-center justify-between'>
@@ -16,47 +32,48 @@ export const DocLayout = (
                <img src='/logo.svg' className='w-7 h-7' />
                <span className='uppercase font-bold text-lg'>Nooo-UI</span>
             </Link>
+            <div className='flex space-x-4 items-center'>
+               <Link
+                  href='https://github.com/nooo-ui/nooo-ui'
+                  target='_blank'
+                  className='p-2.5 font-medium hover:underline underline-offset-2'
+               >
+                  Github
+               </Link>
+            </div>
          </div>
          <div className='fixed w-52 left-60 top-32'>
-            <ul className='border-b pb-4'>
-               <li className='text-gray-900 my-3.5 text-sm font-medium'>
-                  Introduction
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm hover:text-gray-900'>
-                  What is VitePress?
-               </li>
-               <li className='text-sky-600 my-2.5 text-sm hover:text-gray-900'>
-                  Getting Started
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm hover:text-gray-900'>
-                  Configuration
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm hover:text-gray-900'>
-                  Deploying
-               </li>
-            </ul>
-            <ul className='border-b pb-4'>
-               <li className='text-gray-900 my-3.5 text-sm font-medium'>
-                  Introduction
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm'>
-                  What is VitePress?
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm'>Getting Started</li>
-               <li className='text-gray-500 my-2.5 text-sm'>Configuration</li>
-               <li className='text-gray-500 my-2.5 text-sm'>Deploying</li>
-            </ul>
-            <ul className=''>
-               <li className='text-gray-900 my-3.5 text-sm font-medium'>
-                  Introduction
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm'>
-                  What is VitePress?
-               </li>
-               <li className='text-gray-500 my-2.5 text-sm'>Getting Started</li>
-               <li className='text-gray-500 my-2.5 text-sm'>Configuration</li>
-               <li className='text-gray-500 my-2.5 text-sm'>Deploying</li>
-            </ul>
+            {routes.map((item, index) => {
+               return (
+                  <ul
+                     className={clsx(
+                        'pb-4',
+                        index !== routes.length - 1 && 'border-b',
+                     )}
+                     key={item.title}
+                  >
+                     <li className='text-gray-900 my-3.5 text-sm font-medium'>
+                        {item.title}
+                     </li>
+                     {item.children.map((it) => {
+                        return (
+                           <Link href={it.path} key={it.path}>
+                              <li
+                                 className={clsx(
+                                    'my-2.5 text-sm',
+                                    it.path === pathname
+                                       ? 'text-sky-600'
+                                       : 'text-gray-500 hover:text-gray-900',
+                                 )}
+                              >
+                                 {it.title}
+                              </li>
+                           </Link>
+                        )
+                     })}
+                  </ul>
+               )
+            })}
          </div>
          <div className='max-w-screen-2xl mx-auto px-8 py-5 pl-72 mt-16'>
             <div className='px-14 pt-10 pb-14 shadow-xl rounded-2xl'>
