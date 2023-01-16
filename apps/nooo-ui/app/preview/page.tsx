132db6885cb3724fa4dcbdc6809ac72f337c95e1
tolerance-go <892728595@qq.com>
2023-01-16 17:48:47 +0800

    chore

--- apps/nooo-ui/app/preview/page.tsx
+++ apps/nooo-ui/app/preview/page.tsx
@@ -1,55 +1,15 @@
-'use client'
-
 import { widgetsData } from '.data/widgets-data'
-import { MobileContainer } from 'components/WidgetList/MobileContainer'
-import { useThemeContext } from 'components/WidgetList/ThemeContext'
-import { defaultMobileSize } from 'constants/defaultMobileSize'
-import { getDoc } from 'utils/getDoc'
-import { getFullPageHtmlCode } from 'utils/getFullPageHtmlCode'
+import { WidgetPreview } from 'components/WidgetPreview'
+import { PageProps } from 'typings/next'
 
-const PreviewPage = (props: {
-   searchParams: {
-      key: string
-   }
-}) => {
+const PreviewPage = async (props: PageProps) => {
    const target = widgetsData.find(
-      (item) => item.key === props.searchParams.key,
+      (item) => item.key === props.searchParams?.key,
    )
-   const { theme } = useThemeContext()
 
    if (!target) return <div>not match</div>
 
-   if (target.meta.mobile) {
-      return (
-         <div className={'flex justify-center items-center min-h-screen'}>
-            <MobileContainer
-               size={defaultMobileSize}
-               type={
-                  typeof target.meta.mobile === 'object'
-                     ? target.meta.mobile.type
-                     : 'page'
-               }
-            >
-               <iframe
-                  className='w-full h-full'
-                  srcDoc={getDoc({
-                     bodyCss: `background-color: white; overflow-x: hidden; height: ${defaultMobileSize.height}px; width: ${defaultMobileSize.width}px;`,
-                     theme,
-                     data: target,
-                  })}
-               />
-            </MobileContainer>
-         </div>
-      )
-   }
-
-   return (
-      <div
-         dangerouslySetInnerHTML={{
-            __html: getFullPageHtmlCode(target, true),
-         }}
-      ></div>
-   )
+   return <WidgetPreview data={target} />
 }
 
 export default PreviewPage
---
+++ apps/nooo-ui/components/WidgetPreview.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { defaultMobileSize } from 'constants/defaultMobileSize'
+import { WidgetData } from 'typings/widgets'
+import { getDoc } from 'utils/getDoc'
+import { getFullPageHtmlCode } from 'utils/getFullPageHtmlCode'
+import { MobileContainer } from './WidgetList/MobileContainer'
+import { useThemeContext } from './WidgetList/ThemeContext'
+
+export const WidgetPreview = ({ data }: { data: WidgetData }) => {
+   const { theme } = useThemeContext()
+
+   if (data.meta.mobile) {
+      return (
+         <div className={'flex justify-center items-center min-h-screen'}>
+            <MobileContainer
+               size={defaultMobileSize}
+               type={
+                  typeof data.meta.mobile === 'object'
+                     ? data.meta.mobile.type
+                     : 'page'
+               }
+            >
+               <iframe
+                  className='w-full h-full'
+                  srcDoc={getDoc({
+                     bodyCss: `background-color: white; overflow-x: hidden; height: ${defaultMobileSize.height}px; width: ${defaultMobileSize.width}px;`,
+                     theme,
+                     data: data,
+                  })}
+               />
+            </MobileContainer>
+         </div>
+      )
+   }
+
+   return (
+      <div
+         dangerouslySetInnerHTML={{
+            __html: getFullPageHtmlCode(data, true),
+         }}
+      ></div>
+   )
+}
