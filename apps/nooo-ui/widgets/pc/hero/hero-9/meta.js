6630e88d1c734b67ea91a8acb18923472b6056f6
tolerance-go <892728595@qq.com>
2023-01-30 16:15:00 +0800

    chore: add some hero widgets

---
+++ apps/nooo-ui/widgets/pc/hero/hero-9/meta.js
@@ -0,0 +1,14 @@
+/** @type {import('../../../../typings/widgets').WidgetItemMeta} */
+module.exports = {
+   props: {
+      keywords: ['hero'],
+      zhKeywords: ['hero'],
+      type: 'block',
+      platform: 'pc',
+      categories: ['hero'],
+   },
+   createDate: '2023-01-30 00:00:00',
+   updateDate: '2023-01-30 00:00:00',
+   frameHeight: 450,
+   tailwindcssVersion: '3.2.x',
+}
