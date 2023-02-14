3eb7efb919fb54cff819101378425c08a8d34fc2
tolerance-go <892728595@qq.com>
2023-02-14 19:47:33 +0800

    chore

---
+++ apps/nooo-ui/widgets/pc/home/home-6/meta.js
@@ -0,0 +1,15 @@
+/** @type {import('../../../../typings/widgets').WidgetItemMeta} */
+module.exports = {
+   props: {
+      keywords: ['page'],
+      zhKeywords: ['页面'],
+      type: 'page',
+      platform: 'pc',
+      categories: ['home'],
+   },
+   createDate: '2023-02-14 00:00:00',
+   updateDate: '2023-02-14 00:00:00',
+   frameHeight: 'xl',
+   theme: 'dark',
+   tailwindcssVersion: '3.2.x',
+}
